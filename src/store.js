6dd643dfc8700a5736a83e091c9c512e5a12f7c1
Liridon Gashi <doo.nii@hotmail.com>
2024-03-21 17:04:39 +0100

    Adding react persist

--- src/store.js
+++ src/store.js
@@ -1,12 +1,22 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./features/user/userSlice";
 import cartReducer from "./features/cart/cartSlice";
+import storage from "redux-persist/lib/storage";
+import { persistReducer } from "redux-persist";
+import { combineReducers } from "@reduxjs/toolkit";
+
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
+
+const reducer = combineReducers({ user: userReducer, cart: cartReducer });
+
+const persistedReducer = persistReducer(persistConfig, reducer);
 
 const store = configureStore({
-  reducer: {
-    user: userReducer,
-    cart: cartReducer,
-  },
+  reducer: persistedReducer,
 });
 
 export default store;
